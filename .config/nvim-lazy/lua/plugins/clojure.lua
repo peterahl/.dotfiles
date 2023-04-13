fe7c15cff6c01a4a4308446c94458336c46acdb5
Peter Lejon <jp.lejon@protonmail.com>
2023-04-13 23:38:41 +0200

    indentation

--- .config/nvim-lazy/lua/plugins/clojure.lua
+++ .config/nvim-lazy/lua/plugins/clojure.lua
@@ -1,28 +1,27 @@
 return {
 
-	{ "Olical/aniseed" },
+  { "Olical/aniseed" },
 
-	{ "Olical/conjure" },
+  { "Olical/conjure" },
 
-	{
-		"nvim-treesitter/nvim-treesitter",
-		opts = function(_, opts)
-			-- add tsx and treesitter
-			vim.list_extend(opts.ensure_installed, {
-				"clojure",
-			})
-		end,
-	},
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "clojure",
+      })
+    end,
+  },
 
-	{
-		"neovim/nvim-lspconfig",
-		---@class PluginLspOpts
-		opts = {
-			---@type lspconfig.options
-			servers = {
-				-- ["clj-kondo"] = {},
-				clojure_lsp = {},
-			},
-		},
-	},
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        -- ["clj-kondo"] = {},
+        clojure_lsp = {},
+      },
+    },
+  },
 }
