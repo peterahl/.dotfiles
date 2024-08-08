bf8e18efe7fe7dd4382ad378fda4fa5e3e5c0047
Peter Lejon <peter.lejon@voady.com>
2024-08-08 17:09:07 +0200

    added yazi

---
+++ .local/bin/open-yazi.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PANE_OUTPUT=$(tmux capture-pane -p)
+
+RES=$(echo "$PANE_OUTPUT" | rg -e "(?:NOR|INS|SEL)\s+(\S*)\s[^│]* (\d+):*.*" -o --replace '$1 $2')
+
+FILE=$(echo $RES | choose 0)
+LINE=$(echo $RES | choose 1)
+
+
+# Open a floating Tmux window and start Yazi in the current directory
+tmux popup -h 75% -w 75% -E "yazi '$FILE'"
+
+
+
